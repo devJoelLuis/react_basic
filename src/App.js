c15f72e16660545fed62c546a85c996c198c5bc0
Joel-Luis-Pinto1 <joel.luis.pinto1@ibm.com>
2022-02-16 17:37:27 -0300

    transformando app em classe e criando state no componente

--- src/App.js
+++ src/App.js
@@ -2,20 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 
 import Comentario from './components/Comentario';
+import React, { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu Projeto</h1>
-      <Comentario nome="João" email="joão@gmail.com" data={new Date(2020, 3, 19)} >
-         Olá tudo bem
-      </Comentario>
-      <Comentario nome="Maria" email="maria@gmail.com" data={new Date(2020, 3, 21)}>
-         Olá tudo bem sim;
-      </Comentario>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    comentarios: [
+      {
+        nome: 'Joel',
+        email: 'joel.luis.pinto1@ibm.com',
+        data: new Date(2020, 3, 21),
+        message: 'Ola, tudo bem sim ....'
+      }, 
+      {
+        nome: 'Maria',
+        email: 'maria@ibm.com',
+        data: new Date(2022, 1, 21),
+        message: 'Ola, tudo bem?'
+      }
+    ]
+  }
+
+
+   render() {
+    return (
+      <div className="App">
+        <h1>Meu Projeto</h1>
+
+        {this.state.comentarios.map( (c, i) => (
+          <Comentario 
+              key={i}
+              nome={c.nome}
+              email={c.email}
+              data={c.data} >
+         {c.message}
+       </Comentario>
+        ))}
+      </div>
+    );
+   }
 }
 
 export default App;
