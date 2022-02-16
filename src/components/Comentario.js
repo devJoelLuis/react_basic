d0cf646b2c66ddd39a4494db410084d409cf0244
Joel-Luis-Pinto1 <joel.luis.pinto1@ibm.com>
2022-02-16 16:17:13 -0300

    first commit

---
+++ src/components/Comentario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Comentario.css';
+
+const Comentario = () => (
+    <div className='Comentario'>
+        <div>Joel</div>
+        <div>Olá eu sou um componente</div>
+    </div>
+);
+
+export default Comentario;
