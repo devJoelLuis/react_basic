def9c5c7ce51bd1c8002b6c38cf316cbac47dc44
Joel-Luis-Pinto1 <joel.luis.pinto1@ibm.com>
2022-02-17 11:16:20 -0300

    formatação de data e hora no comentário

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { formatRelative } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import './Comentario.css';
 import imgUser from '../assets/user.png'
 
@@ -11,7 +14,7 @@ const Comentario = props => {
             <p class="nome" >{props.nome}</p>
             <p class="email" >{props.email}</p>
             <p class="message" >{props.children}</p>
-            <p class="data" >{props.data.toString()}</p>
+            <p class="data" >{formatRelative(props.data, new Date(), { locale: ptBR })}</p>
             <button onClick={props.onRemove}>&times;</button>  
         </div>    
     </div>
